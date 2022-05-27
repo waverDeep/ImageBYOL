03791c81aa597e96a83a3e826ada3cb18a5f6a6f
waverDeep <dandyleader@naver.com>
2022-05-27 14:32:05 +0900

    update serving code

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ import src.trainers.tester as tester
 app = Flask(__name__)
 
 image_path = './upload'
-config = './config/F10-serving-ImageBYOL-AdamP.json'
-label_list = file_io.read_txt2list('./dataset/INGD_V1-category.txt')
+config = './config/F10-serving-ImageBYOL-ResNet18-AdamP.json'
+label_list = file_io.read_txt2list('./dataset/V3-label.txt')
 
 with open(config, 'r') as configuration:
     config = json.load(configuration)
@@ -33,17 +33,45 @@ print("{}".format(downstream_model))
 downstream_model = downstream_model.cuda()
 
 
+def check_key(input_key):
+    if input_key == '2022y05m22d':
+        return True
+    else:
+        return False
+
+
+
+def check_version(input_version):
+    if input_version == "1":
+        return True
+    else:
+        return False
+
+
+def save_files(file_list):
+    filename_list = []
+    filepath_list = []
+    for file in file_list:
+        filename = secure_filename(file.filename)
+        filename_list.append(filename)
+        filename = os.path.join(image_path, filename)
+        file.save(filename)
+        filepath_list.append(filename)
+    return filename_list, filepath_list
+
 def inference(data):
     downstream_model.eval()
-    data = data.unsqueeze(0)
+    # data = data.unsqueeze(0)
     if config['use_cuda']:
         data = data.cuda()
     with torch.no_grad():
         prediction = downstream_model(data)
         _, predicted = torch.max(prediction.data, 1)
     predicted = predicted.cpu().numpy()
-    output = label_list[predicted[0]]
-    print(output)
+    output = []
+    for pick in predicted:
+        output.append(label_list[pick])
+    return output
 
 
 def load_inference_data(file):
@@ -61,10 +89,32 @@ def load_inference_data(file):
     return data
 
 
-def main(config, file):
+def load_datapack(filelist):
+    datapack = []
+    image_transforms = transforms.Compose(
+        [
+            transforms.Resize(640),
+            transforms.CenterCrop(512),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    )
+
+    for file in filelist:
+        data = PIL.Image.open(file)
+        data = image_transforms(data)
+        datapack.append(data)
+    return torch.stack(datapack)
+
+
+
+
+def main(file):
     data = load_inference_data(file)
     print(data.size())
-    inference(data)
+    output = inference(data)
+    print(output)
+    return output
 
 
 @app.route('/',)
@@ -72,16 +122,44 @@ def predict():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
-def file_upload():
-    gt_file = request.files.get('gt_file')
+@app.route('/multiple_predict', methods=['POST'])
+def multiple_predict():
+    response_part = {"status":"failure", "response_data":{"message": "check querystring parameters"}}
+    if not check_key(request.values.get("appKey")):
+        response_part['status'] = 'failure'
+        response_part['response_data'] = {"message": "appKey error"}
+        print(response_part)
+        return response_part
+    if not check_version(request.values.get("version")):
+        response_part['status']='failure'
+        response_part['response_data'] = {"message": "version error"}
+        print(response_part)
+        return response_part
 
-    gt_filename = secure_filename(gt_file.filename)
-    gt_file.save(os.path.join(image_path, gt_filename))
+    image_files = request.files.getlist("files")
+    if len(image_files) == 0 or image_files[0].filename == '':
+        response_part['status'] = 'failure'
+        response_part['response_data'] = {"message": "image files are not exist"}
+        print(response_part)
+        return response_part
+    filename_list, filepath_list = save_files(image_files)
 
-    main(config, os.path.join(image_path, gt_filename))
+    datapack = load_datapack(filepath_list)
+    model_predicts = inference(datapack)
+    response_predicts = {}
+    # for i in range(len(filename_list)):
+    #     model_predicts.append("output {}".format(i))
 
-    return render_template('predict.html')
+    for name, result in zip(filename_list, model_predicts):
+        response_predicts[name]=result
+
+    if len(response_predicts) is not 0:
+        response_part['status'] = 'success'
+        response_part['response_data'] = {"predicts": response_predicts}
+        print(response_part)
+        return response_part
+    return response_part
+
 
 
 
